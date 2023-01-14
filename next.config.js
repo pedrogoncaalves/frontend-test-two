755dd0f03c7b714071c32bf39dabf0469545ec0a
Pedro Gonçalves <101778028+pedrogoncaalves@users.noreply.github.com>
2023-01-14 18:58:48 -0300

    handling response data

--- next.config.js
+++ next.config.js
@@ -2,7 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  remoteImages: true
+  remoteImages: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.correio24horas.com.br',
+        port: '',
+        pathname: '/fileadmin/user_upload/correio24horas/2022/09/30/040b7839fa9f18bc54c02bab020a8f2a.jpg',
+      },
+    ],
+  },
 }
+
+
+
+ 
+
 
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 import  Header  from "../components/Header";
 import Image from "next/image";
+import Link from "next/link";
 
 
 export default function Home ({ news }) {
@@ -12,15 +13,20 @@ export default function Home ({ news }) {
         <>
         <Header/>
         <div className="container">
+            <main className="main__content">
         <ul>
-            {news.map((n) => (
+            {news.map((n) =>   (
                 <li key={n}>
-                    <Image width="100" height="50" alt="news image" src={n.image}/>
-                    {n.title}
+                    <span>{n.category.toUpperCase()}</span>
+                    <br/>
+                   <Link href={n.url}> {n.title}</Link>
+
+                    
                 
                 </li>
             ))}
         </ul>
+        </main>
         </div>
 
         </>
@@ -29,14 +35,14 @@ export default function Home ({ news }) {
 
 export const getServerSideProps = async () => {
     const apiKey = '109f03afaddf6f94afd50d8945d2c275';
-    const response = await fetch(`http://api.mediastack.com/v1/news?access_key=${apiKey}&countries=br`)
+    const response = await fetch(`http://api.mediastack.com/v1/news?access_key=${apiKey}&countries=us`)
     const primaryData = await response.json();
     const arrayOfNews = primaryData.data
     
 
     const newsTitle = arrayOfNews.map((news) => news);
     const newsImage = arrayOfNews.map((news) => news.image);
-  //  console.log(newsImage)
+  
 
  
 
